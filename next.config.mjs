857e6271913cb29de124fba847efc06e86a5a3ba
Tolulope Adams <tolulopeadams7000@gmail.com>
2024-05-21 14:39:45 +0100

    Update nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/my-portfolio-website",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
